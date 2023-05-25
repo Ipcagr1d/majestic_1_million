c75d75f90425e46435e1b160f71705c9afd220d5
iputucaganesha <iputucaganesha@gmail.com>
2023-05-25 15:29:36 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name  = 'majestic-1-million',
 version     = '0.1',
 packages    = ['majestic'],
 # fork of url = 'http://github.com/davedash/Alexa-Top-Sites',
-url = 'http://github.com/davedash/Alexa-Top-Sites',
+url = 'https://github.com/Ipcagr1d/majestic_1_million',
 author = 'Putu Ganesha',
 author_email = 'ipcagwork.com',
 )
